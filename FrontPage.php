473f196cf5e955f9eac3b3aafeaa9938c5590aff
Ahmed Kamel <ahm.kam92@gmail.com>
2015-01-25 20:54:57 +0200

    Now SignIn from FrontPage!

--- FrontPage.php
+++ FrontPage.php
@@ -12,23 +12,22 @@
     $content_title = "";
     $content = 
 '
-	  <div style="float:left;">
+    <div style="float:left;">
 	
 	<div align="left" style="float:left; width:49%;">
-	    <h2 align="center" style="font-size: 36px; font-weight: bold;">Training Platform</h2>
-	    <!--h3 align="center" style="color:#636363; font-size:20px;">Create your university training platform now!</h3-->
-	    
-	    <br/>
-	    <div align="center"><img width="120" height="120" src="css/images/premium.png" alt=""></div>
-	    <br/>
-	    
-	    <div style="padding: 0 10px 0 10px;">
-		<h3 style="color:#636363; font-size:20px; text-align:center;">Welcome visitor!<br/><br/></h3>
-		<p style="color:#636363; font-size:18px; text-align:justify;">We are proud to announce Coding Door\'s beta version. It has been a long journey till we made it to this point! Thanks to everyone supported us and helped out to bring Coding Door to life!<br/><br/>As a quick start, we\'re just running AAST ACM Training and you will be one of our first users! So we\'ll be waiting for your kind feedbacks and bugs\' reporting, thanks in advance!</p>
-	    </div>
-	    <!--div style="padding-right:10px; "><h3 style=" color:#636363; font-size:20px;">Coding Door currently supports:</h3></div>
-	    <div style="padding-top:13px;"><p>Notice that this process must be done by school owner or director as it will need school verification and payements for premium accounts.</p></div>
-	    <div align="center"><button class="button" onclick="location.href=\'SchoolRegistration.php\';">Register School</button></div-->
+	    <form action="Controllers/UserController.php?functionName=SignIn" method="post">
+		
+		<h2 align="center" style="font-size: 36px; font-weight: bold;">Sign In</h2>
+		<h3 align="center" style="color:#636363; font-size:20px;">Members only!</h3>
+		<br/>
+		<input value="" placeholder="Email" class="textbox empty" type="text" name="email" id="email" required />
+		</br>
+		<input value="" type="password" placeholder="Password" class="textbox empty" id="password" name="password" required/>
+		</br>
+		<label style="color:red; font-weight:bold; font-size:15px">'.$error_message.'</label>
+		</br></br>
+		<div align="center"><input value="Sign In" type="submit" class="button"/></div>
+	    </form>
 	</div>
 
 	<div style="float:left; border-left:1px solid #e3e3e3; height:500px; width:1%;"></div>
--- Home.php
+++ Home.php
@@ -13,8 +13,25 @@
 	if(isset($_SESSION['error_message']) == true)$error_message = $_SESSION['error_message'];
 
 	$title = "Home";
-	$content_title = "Coming Soon!";//"Welcome back ".$_SESSION['user_firstname']." ".$_SESSION['user_lastname']."!";
+	$content_title = "Under Construction";//"Welcome back ".$_SESSION['user_firstname']." ".$_SESSION['user_lastname']."!";
 	$content = '
+	    
+	<div align="center">
+	    <h2 align="center" style="font-size: 36px; font-weight: bold;">Training Platform</h2>
+	    <!--h3 align="center" style="color:#636363; font-size:20px;">Create your university training platform now!</h3-->
+	    
+	    <br/>
+	    <div align="center"><img width="120" height="120" src="css/images/premium.png" alt=""></div>
+	    <br/>
+	    
+	    <div style="padding: 0 10px 0 10px;">
+		<h3 style="color:#636363; font-size:20px; text-align:center;">Welcome visitor!<br/><br/></h3>
+		<p style="color:#636363; font-size:18px; text-align:justify;">We are proud to announce Coding Door\'s beta version. It has been a long journey till we made it to this point! Thanks to everyone supported us and helped out to bring Coding Door to life!<br/><br/>As a quick start, we\'re just running AAST ACM Training and you will be one of our first users! So we\'ll be waiting for your kind feedbacks and bugs\' reporting, thanks in advance!</p>
+	    </div>
+	    <!--div style="padding-right:10px; "><h3 style=" color:#636363; font-size:20px;">Coding Door currently supports:</h3></div>
+	    <div style="padding-top:13px;"><p>Notice that this process must be done by school owner or director as it will need school verification and payements for premium accounts.</p></div>
+	    <div align="center"><button class="button" onclick="location.href=\'SchoolRegistration.php\';">Register School</button></div-->
+	</div>
 
 	<script>var LINK = "homeLINK";</script>
 	';

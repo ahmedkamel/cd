d143648b20d29351858e167dd36b3f2726dd04d8
Omar Ali <en.omarali@gmail.com>
2015-01-27 21:10:02 +0200

    text area fix

--- CreateTutorial.php
+++ CreateTutorial.php
@@ -18,30 +18,9 @@
     $title = "New Tutorial";
     $content_title = "New Tutorial";
     $content = '
-
-        <script type="text/javascript" src="js/tinymce/tinymce.min.js"></script>
-        <script type="text/javascript">
-        tinymce.init({
-            selector: "textarea",
-            theme: "modern",
-            plugins: [
-                "advlist autolink lists link image charmap print preview hr anchor pagebreak",
-                "searchreplace wordcount visualblocks visualchars code fullscreen",
-                "insertdatetime media nonbreaking save table contextmenu directionality",
-                "emoticons template paste textcolor colorpicker textpattern"
-            ],
-            toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
-            toolbar2: "print preview media | forecolor backcolor emoticons",
-            image_advtab: true,
-            templates: [
-                {title: "Test template 1", content: "Test 1"},
-                {title: "Test template 2", content: "Test 2"}
-            ]
-        });
-        </script>
-
+       
         <form method="post" action="somepage">
-            <textarea name="content" style="width:100%; height:400px"></textarea></br>
+            <texteditor name="content" style="width:100%; height:400px"></texteditor></br>
             <input value="Submit" type="submit" name="button" class="button" id="submit"/>
         </form>
 
